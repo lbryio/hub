6206589d9807088f8d1b48a234530be804646506
Jack Robison <jackrobison@lbry.io>
2022-03-31 11:17:49 -0400

    move deploy_scribe_dev.sh to scripts
    -add docker/hooks

--- scripts/deploy_scribe_dev.sh
+++ scripts/deploy_scribe_dev.sh
@@ -3,11 +3,11 @@
 # usage: deploy_scribe_dev.sh <host to update>
 TARGET_HOST=$1
 
-DOCKER_DIR=`dirname $0`
-SCRIBE_DIR=`dirname $DOCKER_DIR`
+SCRIPTS_DIR=`dirname $0`
+SCRIBE_DIR=`dirname $SCRIPTS_DIR`
 
 # build the image
-docker build -f $DOCKER_DIR/Dockerfile -t lbry/scribe:development $SCRIBE_DIR
+docker build -f $SCRIBE_DIR/docker/Dockerfile -t lbry/scribe:development $SCRIBE_DIR
 IMAGE=`docker image inspect lbry/scribe:development | sed -n "s/^.*Id\":\s*\"sha256:\s*\(\S*\)\".*$/\1/p"`
 
 # push the image to the server
