b873f75ff6a2b81e11d66bc010ecaea6d06b34ee
Jack Robison <jackrobison@lbry.io>
2022-05-18 15:08:56 -0400

    update entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 
 case "$HUB_COMMAND" in
   scribe ) exec /home/lbry/.local/bin/scribe "$@" ;;
-  scribe-hub ) exec /home/lbry/.local/bin/herald "$@" ;;
+  herald ) exec /home/lbry/.local/bin/herald "$@" ;;
   scribe-elastic-sync ) exec /home/lbry/.local/bin/scribe-elastic-sync "$@" ;;
   * ) "HUB_COMMAND env variable must be scribe, herald, or scribe-elastic-sync" && exit 1 ;;
 esac
