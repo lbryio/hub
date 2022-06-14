fbe68d516c9bf9ccf880d6be54883899a8b471d2
Jack Robison <jackrobison@lbry.io>
2022-06-14 13:55:53 -0400

    optionally initialize rocksdb from a snapshot
    fixes https://github.com/lbryio/hub/issues/10

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,22 @@
 
 set -euo pipefail
 
+SNAPSHOT_URL="${SNAPSHOT_URL:-}" #off by default. latest snapshot at https://lbry.com/snapshot/hub
+
+if [[ "$HUB_COMMAND" == "scribe" ]] && [[ -n "$SNAPSHOT_URL" ]] && [[ ! -d /database/lbry-rocksdb ]]; then
+  files="$(ls)"
+  echo "Downloading hub snapshot from $SNAPSHOT_URL"
+  wget --no-verbose --trust-server-names --content-disposition "$SNAPSHOT_URL"
+  echo "Extracting snapshot..."
+  filename="$(grep -vf <(echo "$files") <(ls))" # finds the file that was not there before
+  case "$filename" in
+    *.tgz|*.tar.gz|*.tar.bz2 )  tar xvf "$filename" --directory /database ;;
+    *.zip ) unzip "$filename" -d /database/ ;;
+    * ) echo "Don't know how to extract ${filename}. SNAPSHOT COULD NOT BE LOADED" && exit 1 ;;
+  esac
+  rm "$filename"
+fi
+
 if [ -z "$HUB_COMMAND" ]; then
   echo "HUB_COMMAND env variable must be scribe, herald, or scribe-elastic-sync"
   exit 1
