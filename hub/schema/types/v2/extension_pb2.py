df57bf1bf1018ef68ccf2d41da555ce260411789
Jonathan Moody <103143855+moodyjon@users.noreply.github.com>
2022-11-10 15:46:55 -0600

    Bump to latest protobuf.

--- hub/schema/types/v2/extension_pb2.py
+++ hub/schema/types/v2/extension_pb2.py
@@ -11,25 +11,16 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0f\x65xtension.proto\x12\x02pb\"\xd5\x01\n\tStringMap\x12)\n\x06\x66ields\x18\x01 \x03(\x0b\x32\x19.pb.StringMap.FieldsEntry\x1a-\n\x05Value\x12\r\n\x03str\x18\x01 \x01(\tH\x00\x12\r\n\x03int\x18\x02 \x01(\x03H\x00\x42\x06\n\x04type\x1a)\n\x06Values\x12\x1f\n\x02vs\x18\x01 \x03(\x0b\x32\x13.pb.StringMap.Value\x1a\x43\n\x0b\x46ieldsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12#\n\x05value\x18\x02 \x01(\x0b\x32\x14.pb.StringMap.Values:\x02\x38\x01\"1\n\tExtension\x12\x1c\n\x03map\x18\x01 \x01(\x0b\x32\r.pb.StringMapH\x00\x42\x06\n\x04typeb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0f\x65xtension.proto\x12\x02pb\x1a\x1cgoogle/protobuf/struct.proto\">\n\tExtension\x12)\n\x06struct\x18\x01 \x01(\x0b\x32\x17.google.protobuf.StructH\x00\x42\x06\n\x04typeb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'extension_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _STRINGMAP_FIELDSENTRY._options = None
-  _STRINGMAP_FIELDSENTRY._serialized_options = b'8\001'
-  _STRINGMAP._serialized_start=24
-  _STRINGMAP._serialized_end=237
-  _STRINGMAP_VALUE._serialized_start=80
-  _STRINGMAP_VALUE._serialized_end=125
-  _STRINGMAP_VALUES._serialized_start=127
-  _STRINGMAP_VALUES._serialized_end=168
-  _STRINGMAP_FIELDSENTRY._serialized_start=170
-  _STRINGMAP_FIELDSENTRY._serialized_end=237
-  _EXTENSION._serialized_start=239
-  _EXTENSION._serialized_end=288
+  _EXTENSION._serialized_start=53
+  _EXTENSION._serialized_end=115
 # @@protoc_insertion_point(module_scope)
